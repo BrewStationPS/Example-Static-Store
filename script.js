6915a33b2a943612404810765faa8f308c0a3f40
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2026-08-11 19:27:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $.getJSON('./listings.json', function (json) {
         $.each(json, function (i, item) {
             var tr = $('<tr/>');
-            tr.append("<td><img src=\"./icons/" + item.titleid + ".png\" width=\"256px\" /></td>");
+            tr.append("<td><img src=\"./icons/" + item.titleid + ".png\" width=\"64px\" /></td>");
             tr.append("<td>" + item.title + "</td>");
             tr.append("<td>" + item.titleid + "</td>");
             tr.append("<td>" + item.author + "</td>");
             tr.append("<td>" + item.platform + "</td>");
-            tr.append("<td>" + item.download + "</td>");
+            if (item.externaldl) tr.append("<td><a href=\"" + item.download + "\">External</a></td>");
             $('table').append(tr);
         });
     }).fail(function (jqxhr, textStatus, error) {
