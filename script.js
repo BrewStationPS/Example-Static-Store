1e1e9e6e2a167a680fdaf2161966cbc8be4fd165
Harommel <124393154+HarommelRabbid@users.noreply.github.com>
2026-08-13 20:16:07 +0300

    Added titleid

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         $.each(json, function (i, item) {
             var tr = $('<tr/>');
             tr.append("<td><img src=\"./icons/" + item.titleid + ".png\" width=\"64px\" /></td>");
+            tr.append("<td>" + item.titleid + "</td>");
             tr.append("<td>" + item.title + "</td>");
             tr.append("<td>" + item.version + "</td>");
             tr.append("<td>" + item.category + "</td>");
